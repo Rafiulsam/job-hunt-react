5792a9b942f09c0119b2494baa54f8876b20880d
Rafiulsam <rafiulsam69@gmail.com>
2024-02-18 15:59:50 +0600

    made responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Header.css"
 import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <nav className='header'>
-            <Link to='/'><h1>JobHut</h1></Link>
-            <div className='navLinks'>
-                <NavLink className={({isActive})=>isActive ? 'active': ''} to='statistics'>Statistics</NavLink>
-                <NavLink to='appliedJobs'>Applied Jobs</NavLink>
-                <NavLink to='blog'>Blog</NavLink>
-            </div>
-            <button className='apply-btn'>Star Applying</button>
-        </nav>
+        <>
+            <nav className='header'>
+                <Link to='/'><h1>JobHunt</h1></Link>
+                <div className='navLinks'>
+                    <NavLink className={({ isActive }) => isActive ? 'active' : ''} to='statistics'>Statistics</NavLink>
+                    <NavLink to='appliedJobs'>Applied Jobs</NavLink>
+                    <NavLink to='blog'>Blog</NavLink>
+                </div>
+                <button className='apply-btn'>Star Applying</button>
+
+            </nav>
+        </>
     );
 };
 
